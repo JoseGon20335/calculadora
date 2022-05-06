79ab78aefd60d8c38a8eda74bb6e661c19bb37a9
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-05 19:47:18 -0600

    calculadora terminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-        
-      <div className="background">
-        
-        <div className="header">
+const App = () => {
 
-        </div>
+  const [resul, setResul] = useState("");
 
-        <div className="MainContent">
-            {
-                
-            }
-        </div>
-      </div>
-    );
+  const handleClick = (e) => {
+    setResul(resul.concat(e.target.name));
   }
+
+  const clear = () => {
+    setResul("");
+  }
+
+  const borrar = () => {
+    setResul(resul.slice(0,resul.length-1));
+  }
+
+  const calcularClick = () => {
+    setResul(eval(resul));
+  }
+
+  return (
+    <div className="calcu">
+      <form>
+        <input type="text" value={resul} />
+      </form>
+      <div className='teclado'>
+        <button className='especial' onClick={clear}>Clear</button>
+        <button className='boton' onClick={borrar}>C</button>
+        <button className='boton' name='/' onClick={handleClick}>/</button>
+        <button className='boton' name='7' onClick={handleClick}>7</button>
+        <button className='boton' name='8' onClick={handleClick}>8</button>
+        <button className='boton' name='9' onClick={handleClick}>9</button>
+        <button className='boton' name='*' onClick={handleClick}>*</button>
+        <button className='boton' name='4' onClick={handleClick}>4</button>
+        <button className='boton' name='5' onClick={handleClick}>5</button>
+        <button className='boton' name='6' onClick={handleClick}>6</button>
+        <button className='boton' name='-' onClick={handleClick}>-</button>
+        <button className='boton' name='1' onClick={handleClick}>1</button>
+        <button className='boton' name='2' onClick={handleClick}>2</button>
+        <button className='boton' name='3' onClick={handleClick}>3</button>
+        <button className='boton' name='+' onClick={handleClick}>+</button>
+        <button className='boton' name='0' onClick={handleClick}>0</button>
+        <button className='boton' name='.' onClick={handleClick}>.</button>
+        <button className='especial2' onClick={calcularClick}>=</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
