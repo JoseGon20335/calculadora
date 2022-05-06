a2809742225384517ad6c0fab2e98049ce4c60c6
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-05 21:37:23 -0600

    fixes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,71 @@ const App = () => {
   }
 
   const calcularClick = () => {
-    setResul(eval(resul));
+
+    let valor = eval(resul)
+
+    let size = valor.toString().length
+
+    console.log(size)
+
+    if(valor < 0){
+      valor = 'ERROR**'
+    } else if(valor > 999999999){
+      valor = 'ERROR'
+    } else{
+      if(size >= 10){
+        let varW = true
+        while(varW == true){
+          if(size == 9){
+            varW = false
+          }else{
+
+            console.log("quitar num1",valor)
+  
+            valor = valor.toString().slice(0,-1)
+            size = size - 1;
+
+            console.log("quitar num2",valor)
+  
+          }
+        }
+      } 
+    }
+    setResul(valor);
+  }
+
+  const negativador = () => {
+
+    let valor = eval(resul)
+
+    let size = valor.toString().length
+
+    console.log(size)
+
+    if(valor < 0){
+      valor = 'ERROR**'
+    } else if(valor > 999999999){
+      valor = 'ERROR'
+    } else{
+      if(size >= 10){
+        let varW = true
+        while(varW == true){
+          if(size == 9){
+            varW = false
+          }else{
+
+            console.log("quitar num1",valor)
+  
+            valor = valor.toString().slice(0,-1)
+            size = size - 1;
+
+            console.log("quitar num2",valor)
+  
+          }
+        }
+      } 
+    }
+    setResul(valor);
   }
 
   return (
@@ -28,23 +92,24 @@ const App = () => {
       </form>
       <div className='teclado'>
         <button className='especial' onClick={clear}>Clear</button>
-        <button className='boton' onClick={borrar}>C</button>
-        <button className='boton' name='/' onClick={handleClick}>/</button>
+        <button className='botonColorido' onClick={borrar}>C</button>
+        <button className='botonColorido' name='/' onClick={handleClick}>/</button>
         <button className='boton' name='7' onClick={handleClick}>7</button>
         <button className='boton' name='8' onClick={handleClick}>8</button>
         <button className='boton' name='9' onClick={handleClick}>9</button>
-        <button className='boton' name='*' onClick={handleClick}>*</button>
+        <button className='botonColorido' name='*' onClick={handleClick}>*</button>
         <button className='boton' name='4' onClick={handleClick}>4</button>
         <button className='boton' name='5' onClick={handleClick}>5</button>
         <button className='boton' name='6' onClick={handleClick}>6</button>
-        <button className='boton' name='-' onClick={handleClick}>-</button>
+        <button className='botonColorido' name='-' onClick={handleClick}>-</button>
         <button className='boton' name='1' onClick={handleClick}>1</button>
         <button className='boton' name='2' onClick={handleClick}>2</button>
         <button className='boton' name='3' onClick={handleClick}>3</button>
-        <button className='boton' name='+' onClick={handleClick}>+</button>
+        <button className='botonColorido' name='+' onClick={handleClick}>+</button>
         <button className='boton' name='0' onClick={handleClick}>0</button>
         <button className='boton' name='.' onClick={handleClick}>.</button>
-        <button className='especial2' onClick={calcularClick}>=</button>
+        <button className='botonColorido' onClick={negativador}>+/-</button>
+        <button className='botonColorido' onClick={calcularClick}>=</button>
       </div>
     </div>
   );
