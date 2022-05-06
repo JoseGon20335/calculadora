b15c87807ed8f49c1728d4a9de0c07d4f4cfa351
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-05-05 23:22:54 -0600

    workandfix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    ecmaVersion: 'latest',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: [
-    "import/prefer-default-export": "off",
-  ],
-};
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
